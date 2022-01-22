7249ac7cd7f79046d88da473f3d64fc71b77ee8f
benjaminbjm <benjamindaygu@gmail.com>
2022-01-22 12:34:24 -0300

    inserindo o projeto Tratar_ErrosJS

---
+++ debugging_Error.js
@@ -0,0 +1,29 @@
+function validarArray(arr, num) {
+   try{
+    if(!arr && !num) throw new ReferenceError("Envie os parâmetros");
+
+    if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object");
+
+    if(typeof num !== 'number') throw new TypeError('Array precisa ser do tipo number');
+
+    if(arr.length !== num) throw new RangeError('Tamanho inválido!');
+    return arr;
+   }
+   catch (e){
+       if (e instanceof ReferenceError) {
+           console.log("Este erro é um RefernceError!")
+           console.log(e .message)
+       } else if (e instanceof TypeError) {
+           console.log("Este errro é um TypeError!")
+           console.log( e .message)
+       } else if (e instanceof RangeError) {
+           console.log("Este erro é um RangeError!")
+           console.log(e .message)
+       } else {
+           console.log("Tipo de erro não esperado: " + e)
+       }
+
+   }
+}
+
+console.log(validarArray([3, 4, 5], 0));
